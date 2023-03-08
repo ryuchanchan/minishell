7de54263e4551717aebb001d4fb43360bd249a10
Toru Yoshida <44675166+hidaytours@users.noreply.github.com>
2023-03-08 19:30:43 +0900

    delete comment

--- src/utils/quotes.c
+++ src/utils/quotes.c
@@ -1,5 +1,5 @@
 #include "quotes.h"
-/*　閉じがない引用符もstatusを変更してしまう、以降に閉じが存在するか確認する処理を追加するかしないか?　*/
+
 bool	is_quote_end(const t_status_quotes status, const char *src, size_t i)
 {
 	if (status == IN_SINGLE_QUOTE && src[i] == '\'')

53a3f99d6b3acd26caa86edd7016df69a8f85cdd
Toru Yoshida <toryoshi@c1r1s7.42tokyo.jp>
2023-03-23 13:51:20 +0900

    fix norm err

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,97 +6,97 @@
 /*   By: rykawamu </var/mail/rykawamu>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 22:40:12 by rykawamu          #+#    #+#             */
-/*   Updated: 2023/03/22 22:42:15 by rykawamu         ###   ########.fr       */
+/*   Updated: 2023/03/23 13:46:42 by toryoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ms_builtin.h"
 
-static void env_set(char *key, char *value, char ***envp_p)
+static void	env_set(char *key, char *value, char ***envp_p)
 {
-    char    *tmp;
-    char    *joined;
-    t_kv    *kv_p;
+	char	*tmp;
+	char	*joined;
+	t_kv	*kv_p;
 
-    tmp = ft_strjoin(key, "=");
-    if (!tmp)
-        fatal_error("cd");
-    joined = ft_strjoin(tmp, value);
-    free(tmp);
-    if (!joined)
-        fatal_error("cd");
-    kv_p = construct_kv(joined);
-    free(joined);
-    if (!kv_p)
-        fatal_error("cd");
-    env_add(envp_p, kv_p);
-    destruct_kv(&kv_p);
+	tmp = ft_strjoin(key, "=");
+	if (!tmp)
+		fatal_error("cd");
+	joined = ft_strjoin(tmp, value);
+	free(tmp);
+	if (!joined)
+		fatal_error("cd");
+	kv_p = construct_kv(joined);
+	free(joined);
+	if (!kv_p)
+		fatal_error("cd");
+	env_add(envp_p, kv_p);
+	destruct_kv(&kv_p);
 }
 
-static  char *get_pwd(char **envp)
+static char	*get_pwd(char **envp)
 {
-    t_kv    *kv_p;
-    char    *dupped;
+	t_kv	*kv_p;
+	char	*dupped;
 
-    kv_p = env_find((const char **)envp, "PWD");
-    if (!kv_p)
-        return (ft_strdup(""));
-    else
-    {
-        dupped = ft_strdup(kv_p->value);
-        destruct_kv(&kv_p);
-        return (dupped);
-    }
+	kv_p = env_find((const char **)envp, "PWD");
+	if (!kv_p)
+		return (ft_strdup(""));
+	else
+	{
+		dupped = ft_strdup(kv_p->value);
+		destruct_kv(&kv_p);
+		return (dupped);
+	}
 }
 
-static int  cd_without_arg(char **envp)
+static int	cd_without_arg(char **envp)
 {
-    t_kv    *kv_p;
+	t_kv	*kv_p;
 
-    kv_p = env_find((const char **)envp, "HOME");
-    if (!kv_p)
-    {
-        ft_putendl_fd("cd: HOME not set", STDERR_FILENO);
-        return (1);
-    }
-    if (chdir(kv_p->value) == -1)
-    {
-        perror("cd");
-        destruct_kv(&kv_p);
-        return (1);
-    }
-    destruct_kv(&kv_p);
-    return (0);
+	kv_p = env_find((const char **)envp, "HOME");
+	if (!kv_p)
+	{
+		ft_putendl_fd("cd: HOME not set", STDERR_FILENO);
+		return (1);
+	}
+	if (chdir(kv_p->value) == -1)
+	{
+		perror("cd");
+		destruct_kv(&kv_p);
+		return (1);
+	}
+	destruct_kv(&kv_p);
+	return (0);
 }
 
-static int cd_with_arg(char *arg)
+static int	cd_with_arg(char *arg)
 {
-    if (chdir(arg) == -1)
-    {
-        perror("cd");
-        return (1);
-    }
-    return (0);
+	if (chdir(arg) == -1)
+	{
+		perror("cd");
+		return (1);
+	}
+	return (0);
 }
 
 int	builtin_cd(char **args, char ***envp_p)
 {
-    int     ret;
-    char    *oldpwd;
-    char    buf_pwd[PATH_MAX];
+	int		ret;
+	char	*oldpwd;
+	char	buf_pwd[PATH_MAX];
 
-    oldpwd = get_pwd(*envp_p);
-    if (!args[1])
-        ret = cd_without_arg(*envp_p);
-    else
-        ret = cd_with_arg(args[1]);
-    if (ret == 0)
-    {
-        if (!getcwd(buf_pwd, PATH_MAX))
-            fatal_error("cd");
-        env_set("OLDPWD", oldpwd, envp_p);
-        env_set("PWD", buf_pwd, envp_p);
-    }
-    free(oldpwd);
-    return (ret);
+	oldpwd = get_pwd(*envp_p);
+	if (!args[1])
+		ret = cd_without_arg(*envp_p);
+	else
+		ret = cd_with_arg(args[1]);
+	if (ret == 0)
+	{
+		if (!getcwd(buf_pwd, PATH_MAX))
+			fatal_error("cd");
+		env_set("OLDPWD", oldpwd, envp_p);
+		env_set("PWD", buf_pwd, envp_p);
+	}
+	free(oldpwd);
+	return (ret);
 }
--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,23 +6,23 @@
 /*   By: rykawamu </var/mail/rykawamu>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 22:42:35 by rykawamu          #+#    #+#             */
-/*   Updated: 2023/03/23 01:42:57 by rykawamu         ###   ########.fr       */
+/*   Updated: 2023/03/23 13:45:06 by toryoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ms_builtin.h"
 
-int builtin_pwd(char **args, char ***envp_p)
+int	builtin_pwd(char **args, char ***envp_p)
 {
-    char buf[PATH_MAX];
-    
-    (void)envp_p;
-    (void)args;
-    if (getcwd(buf, PATH_MAX) == NULL)
-    {
-        perror("pwd");
-        return (1);
-    }
-    ft_printf("%s\n", buf);
-    return (0);
+	char	buf[PATH_MAX];
+
+	(void)envp_p;
+	(void)args;
+	if (getcwd(buf, PATH_MAX) == NULL)
+	{
+		perror("pwd");
+		return (1);
+	}
+	ft_printf("%s\n", buf);
+	return (0);
 }
